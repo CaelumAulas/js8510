554a7fdd435dbebb109b7b51c249e300c768c458
Artur Diniz Adam <artur.daz@gmail.com>
2019-08-14 11:44:09 -0300

    Curiosidades do EE

--- scripts/navegacao/barraDeEndereco.js
+++ scripts/navegacao/barraDeEndereco.js
@@ -1,3 +1,5 @@
+import { carregar } from '/scripts/navegacao/carregar.js'
+
 function mudaEnderecoResumido() {
     $inputEndereco.value = $iframeJanela.contentWindow.location.hostname
 }
@@ -12,3 +14,25 @@ $inputEndereco.addEventListener('blur', mudaEnderecoResumido)
 // $iframeJanela é um EventEmmiter
 $iframeJanela.addEventListener("load", mudaEnderecoResumido)
 
+// Parâmetro é opcional
+$inputEndereco.addEventListener('keyup', function carregarDigitado() {
+    const evento = arguments[0]
+    const queNaoExiste = arguments[1]
+    
+    console.log(arguments)
+    console.log("Nº de argumentos", arguments.length)
+    console.log("Nao existe: ", queNaoExiste)
+    console.log(evento)
+
+    if(evento.key === 'Enter') {        
+        const enderecoDigitado = this.value
+        carregar(enderecoDigitado)
+    }
+})
+
+
+// Em algum momento
+// Eu sou o chrome
+// const tecla = "Enter"
+// const evento = objetao
+// carregarDigitado.call($inputEndereco, evento)
